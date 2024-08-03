4cfa2bf7d9506a005618a80334b5a17bcb5ffc8a
valterlobo <valterlobo@gmail.com>
2024-08-03 20:18:19 -0300

    feat: Desafio contador

---
+++ controle-fluxo-contador/src/main/java/org/dio/desafio/contador/valterlobo/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package org.dio.desafio.contador.valterlobo;
+
+public class ParametrosInvalidosException extends  Exception {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public ParametrosInvalidosException() {
+        super();
+    }
+
+}
