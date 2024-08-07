0cada98ea897d666a8d163105013e8d723bd0d74
valterlobo <valterlobo@gmail.com>
2024-08-07 00:13:56 -0300

    Mentoria e curso

---
+++ desafio-poo/src/main/java/br/com/dio/desafio/poo/dominio/Curso.java
@@ -0,0 +1,9 @@
+package br.com.dio.desafio.poo.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+}
---
+++ desafio-poo/src/main/java/br/com/dio/desafio/poo/dominio/Mentoria.java
@@ -0,0 +1,13 @@
+package br.com.dio.desafio.poo.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    private LocalDate dataCriacao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+}
